e35ae5afc27557263617033c765c427d80059074
zsh <390447783@qq.com>
2020-09-09 18:08:35 +0800

    feat: show res

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Radio, Typography, Form, Input, Button } from 'antd';
+import { Radio, Typography, Form, Input, Button, Result } from 'antd';
 import {Flex} from 'rebass'
 import "../styles/card.css"
 import { UserOutlined, PhoneOutlined } from '@ant-design/icons';
@@ -20,14 +20,45 @@ const listInit = [
 
 const MainContent = () => {
   const [list, setList] = useState([]);
+  const [showRes, setShowRes] = useState(false);
+  const [resStatus, setResStatus] = useState({
+    type: 'warning',
+    msg: '服务器开小差~'
+  });
 
   const onFinish = async values => {
     console.log('Success:', values);
     const {name, phone, session} = values;
-    const res = await axios.post('/baoming/',{
-      name, phone, session
-    });
-    console.log(res);
+    try {
+      const res = await axios.post('/baoming/',{
+        name, phone, session
+      });
+      console.log(res);
+      if(res.data === 0) {
+        setResStatus({
+          type: 'success',
+          msg: '成功'
+        })
+      } else if(res.data === 1) {
+        setResStatus({
+          type: 'info',
+          msg: '该场次已满'
+        })
+      } else {
+        setResStatus({
+          type: 'warning',
+          msg: '服务器开小差~'
+        })
+      }
+      setShowRes(true)
+    }catch (e) {
+      console.log(e)
+      setResStatus({
+        type: 'warning',
+        msg: '服务器开小差~'
+      })
+      setShowRes(true)
+    }
   };
 
   useEffect(() => {
@@ -57,69 +88,84 @@ const MainContent = () => {
   }, [])
 
   return (
-    <Flex bg='#fff' className='card_contain' sx={{minHeight: ['100vh', '100vh', 'auto']}}>
-        <Flex width={1} className='height100' flexDirection="column">
-          <div style={{marginBottom: 50}}>
-            <Title level={4}>建发磐龙府中秋博饼预约</Title>
-            <Text  type="secondary">选择您所需要预约的信息，我们将在第一时间为您安排。</Text>
-          </div>
-          <Form
-            name="basic"
-            onFinish={onFinish}
-          >
-            <Form.Item
-              labelCol={{span: 24}}
-              label={<Text strong>姓名</Text>}
-              name="name"
-              rules={[{ required: true, message: '必填项' }]}
+    <>
+      {showRes ? (
+        <Result
+          status={resStatus.type}
+          title={resStatus.msg}
+          extra={resStatus.type !== 'success' &&
+            <Button type="primary" onClick={() => setShowRes(false)}>
+              重选
+            </Button>
+          }
+        />
+      ) : (
+        <Flex bg='#fff' className='card_contain' sx={{minHeight: ['100vh', '100vh', 'auto']}}>
+          <Flex width={1} className='height100' flexDirection="column">
+            <div style={{marginBottom: 50}}>
+              <Title level={4}>建发磐龙府中秋博饼预约</Title>
+              <Text  type="secondary">选择您所需要预约的信息，我们将在第一时间为您安排。</Text>
+            </div>
+            <Form
+              name="basic"
+              onFinish={onFinish}
             >
-              <Input prefix={<UserOutlined />} />
-            </Form.Item>
-            <Form.Item
-              labelCol={{span: 24}}
-              label={<Text strong>手机</Text>}
-              name="phone"
-              rules={[{
-                required: true, message: '必填项'
-              },
-              {
-                pattern: /^1\d{10}( 1\d{10})*$/,
-                message: '手机号格式不正确',
-              }
-              ]}
-            >
-              <Input prefix={<PhoneOutlined />} />
-            </Form.Item>
+              <Form.Item
+                labelCol={{span: 24}}
+                label={<Text strong>姓名</Text>}
+                name="name"
+                rules={[{ required: true, message: '必填项' }]}
+              >
+                <Input prefix={<UserOutlined />} />
+              </Form.Item>
+              <Form.Item
+                labelCol={{span: 24}}
+                label={<Text strong>手机</Text>}
+                name="phone"
+                rules={[{
+                  required: true, message: '必填项'
+                },
+                  {
+                    pattern: /^1\d{10}( 1\d{10})*$/,
+                    message: '手机号格式不正确',
+                  }
+                ]}
+              >
+                <Input prefix={<PhoneOutlined />} />
+              </Form.Item>
 
-            <br/>
-            <Form.Item
-              labelCol={{span: 24}}
-              label={<Text strong>博饼日期</Text>}
-              name="session"
-              rules={[{ required: true, message: '必填项' }]}
-            >
-              <Radio.Group value={1}>
-                {list.map(item => (
-                  list.length === 3 ?
-                  <Radio key={item.session} className="radio_style" value={item.session}>
-                    测试{item.session}（剩余{item.count}）
-                  </Radio> : <Radio key={item.session} className="radio_style" value={item.session}>
-                      测试{item.session}（剩余{item.count}）
-                    </Radio>
-                ))}
-              </Radio.Group>
-            </Form.Item>
-            <Form.Item>
-              <Flex justifyContent="center">
-                <Button type="primary" htmlType="submit">
-                  提交
-                </Button>
-              </Flex>
+              <br/>
+              <Form.Item
+                labelCol={{span: 24}}
+                label={<Text strong>博饼日期</Text>}
+                name="session"
+                rules={[{ required: true, message: '必填项' }]}
+              >
+                <Radio.Group value={1}>
+                  {list.map(item => (
+                    list.length === 3 ?
+                      <Radio key={item.session} className="radio_style" value={item.session}>
+                        测试{item.session}（剩余{item.count}）
+                      </Radio> : <Radio key={item.session} className="radio_style" value={item.session}>
+                        测试{item.session}（剩余{item.count}）
+                      </Radio>
+                  ))}
+                </Radio.Group>
+              </Form.Item>
+              <Form.Item>
+                <Flex justifyContent="center">
+                  <Button type="primary" htmlType="submit">
+                    提交
+                  </Button>
+                </Flex>
 
-            </Form.Item>
-          </Form>
+              </Form.Item>
+            </Form>
+          </Flex>
         </Flex>
-    </Flex>
+      )}
+
+    </>
   )
 }
 
