480ba116a3e42a9ddf8f1ebcd30cbc67eb070637
zsh <390447783@qq.com>
2020-09-09 17:49:59 +0800

    fix: bug

--- components/MainContent.js
+++ components/MainContent.js
@@ -7,6 +7,17 @@ import axios from 'axios'
 
 
 const { Title, Text } = Typography;
+
+const listInit = [
+  '央著16日',
+  '央著17日',
+  '央誉18日',
+  '央誉19日',
+  '玉湖壹号20日',
+  '磐龙府21日',
+  '磐龙府22日'
+]
+
 const MainContent = () => {
   const [list, setList] = useState([]);
 
@@ -24,10 +35,23 @@ const MainContent = () => {
       try {
         const res = await axios.post('/shuliang/');
         console.log(res)
-        setList(res.data)
+        setList(res.data.map(i => {
+          const newI = i;
+          newI.title = listInit[i.session];
+          return newI;
+        }))
       }catch (e) {
         console.log(e)
-        setList([{count: 0, session: 0}, {count: 5, session: 1}, {count: 1, session: 2}, {count: 0, session: 3}, {count: 0, session: 4}, {count: 0, session: 5}, {count: 0, session: 6}])
+        setList([
+            {
+              title: '测试', count: 0, session: 0
+            }, {
+              title: '测试', count: 5, session: 1
+            }, {
+              title: '测试', count: 1, session: 2
+          }
+          ]
+        )
       }
     })()
   }, [])
@@ -39,8 +63,6 @@ const MainContent = () => {
             <Title level={4}>建发磐龙府中秋博饼预约</Title>
             <Text  type="secondary">选择您所需要预约的信息，我们将在第一时间为您安排。</Text>
           </div>
-
-
           <Form
             name="basic"
             onFinish={onFinish}
@@ -57,12 +79,19 @@ const MainContent = () => {
               labelCol={{span: 24}}
               label={<Text strong>手机</Text>}
               name="phone"
-              rules={[{ required: true, message: '必填项' }]}
+              rules={[{
+                required: true, message: '必填项'
+              },
+              {
+                pattern: /^1\d{10}( 1\d{10})*$/,
+                message: '手机号格式不正确',
+              }
+              ]}
             >
               <Input prefix={<PhoneOutlined />} />
             </Form.Item>
 
-
+            <br/>
             <Form.Item
               labelCol={{span: 24}}
               label={<Text strong>博饼日期</Text>}
@@ -71,14 +100,15 @@ const MainContent = () => {
             >
               <Radio.Group value={1}>
                 {list.map(item => (
+                  list.length === 3 ?
                   <Radio key={item.session} className="radio_style" value={item.session}>
                     测试{item.session}（剩余{item.count}）
-                  </Radio>
+                  </Radio> : <Radio key={item.session} className="radio_style" value={item.session}>
+                      测试{item.session}（剩余{item.count}）
+                    </Radio>
                 ))}
               </Radio.Group>
             </Form.Item>
-
-
             <Form.Item>
               <Flex justifyContent="center">
                 <Button type="primary" htmlType="submit">
