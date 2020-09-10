08a88422f331f0b9403089724b47788e12340ec7
zsh <390447783@qq.com>
2020-09-10 19:05:32 +0800

    fix: fix

--- components/MainContent.js
+++ components/MainContent.js
@@ -97,7 +97,7 @@ const MainContent = () => {
 
   return (
     <>
-      {showRes ? (
+      {showRes && (
         <Result
           status={resStatus.type}
           title={resStatus.msg}
@@ -107,163 +107,158 @@ const MainContent = () => {
             </Button>
           }
         />
-      ) : (
-        <Flex bg='#fff' className='card_contain' sx={{minHeight: ['100vh', '100vh', 'auto']}}>
-          <Flex width={1} className='height100' flexDirection="column">
-            <div style={{marginBottom: 50}}>
-              <Title level={4}>建发磐龙府中秋博饼预约</Title>
-              <Text  type="secondary">选择您所需要预约的信息，我们将在第一时间为您安排。</Text>
-            </div>
-            <Form
-              name="basic"
-              onFinish={onFinish}
+      )}
+      <Flex style={{display: showRes && 'none'}} bg='#fff' className='card_contain' sx={{minHeight: ['100vh', '100vh', 'auto']}}>
+        <Flex width={1} className='height100' flexDirection="column">
+          <div style={{marginBottom: 50}}>
+            <Title level={4}>建发磐龙府中秋博饼预约</Title>
+            <Text  type="secondary">选择您所需要预约的信息，我们将在第一时间为您安排。</Text>
+          </div>
+          <Form
+            name="basic"
+            onFinish={onFinish}
+          >
+            <Form.Item
+              labelCol={{span: 24}}
+              label={<Text strong>姓名</Text>}
+              name="name"
+              rules={[{ required: true, message: '必填项' }]}
             >
+              <Input prefix={<UserOutlined />} />
+            </Form.Item>
+            <Form.Item
+              labelCol={{span: 24}}
+              label={<Text strong>手机</Text>}
+              name="phone"
+              rules={[{
+                required: true, message: '必填项'
+              },
+                {
+                  pattern: /^1\d{10}( 1\d{10})*$/,
+                  message: '手机号格式不正确',
+                }
+              ]}
+            >
+              <Input prefix={<PhoneOutlined />} />
+            </Form.Item>
+            <Form.Item
+              labelCol={{span: 24}}
+              label={<Text strong>房号</Text>}
+              name="room"
+              rules={[
+                { required: true, message: '必填项' },
+                {
+                  pattern: /^[0-9]+#[0-9]+$/,
+                  message: '房号格式不正确',
+                }
+              ]}
+            >
+              <Input placeholder="1#203" prefix={<HomeOutlined />} />
+            </Form.Item>
+            <br/>
+
+
+            <Form.Item
+              labelCol={{span: 24}}
+              label={<Text strong>博饼日期</Text>}
+            >
+              {/*<Radio.Group value={1}>*/}
+              {/*  {list.map(item => (*/}
+              {/*    <Radio key={item.session} className="radio_style" value={item.session}>*/}
+              {/*      {item.title}（剩余{item.count}）*/}
+              {/*    </Radio>*/}
+              {/*  ))}*/}
+              {/*</Radio.Group>*/}
+
               <Form.Item
                 labelCol={{span: 24}}
-                label={<Text strong>姓名</Text>}
-                name="name"
+                name="xiaoqu"
                 rules={[{ required: true, message: '必填项' }]}
               >
-                <Input prefix={<UserOutlined />} />
+                <Select placeholder="选择小区" onChange={val => setCurrentRoom(val)}>
+                  <Option value="0">央著</Option>
+                  <Option value="1">央誉</Option>
+                  <Option value="2">玉湖壹号</Option>
+                  <Option value="3">磐龙府</Option>
+                </Select>
               </Form.Item>
-              <Form.Item
-                labelCol={{span: 24}}
-                label={<Text strong>手机</Text>}
-                name="phone"
-                rules={[{
-                  required: true, message: '必填项'
-                },
-                  {
-                    pattern: /^1\d{10}( 1\d{10})*$/,
-                    message: '手机号格式不正确',
-                  }
-                ]}
-              >
-                <Input prefix={<PhoneOutlined />} />
-              </Form.Item>
-              <Form.Item
-                labelCol={{span: 24}}
-                label={<Text strong>房号</Text>}
-                name="room"
-                rules={[
-                  { required: true, message: '必填项' },
-                  {
-                    pattern: /^[0-9]+#[0-9]+$/,
-                    message: '房号格式不正确',
-                  }
-                  ]}
-              >
-                <Input placeholder="1#203" prefix={<HomeOutlined />} />
-              </Form.Item>
-              <br/>
-
-
-
-
 
               <Form.Item
                 labelCol={{span: 24}}
-                label={<Text strong>博饼日期</Text>}
+                name="session"
+                rules={[{ required: true, message: '必填项' }]}
               >
-                {/*<Radio.Group value={1}>*/}
-                {/*  {list.map(item => (*/}
-                {/*    <Radio key={item.session} className="radio_style" value={item.session}>*/}
-                {/*      {item.title}（剩余{item.count}）*/}
-                {/*    </Radio>*/}
-                {/*  ))}*/}
-                {/*</Radio.Group>*/}
-
-                <Form.Item
-                  labelCol={{span: 24}}
-                  name="xiaoqu"
-                  rules={[{ required: true, message: '必填项' }]}
-                >
-                  <Select placeholder="选择小区" onChange={val => setCurrentRoom(val)}>
-                    <Option value="0">央著</Option>
-                    <Option value="1">央誉</Option>
-                    <Option value="2">玉湖壹号</Option>
-                    <Option value="3">磐龙府</Option>
-                  </Select>
-                </Form.Item>
-
-                <Form.Item
-                  labelCol={{span: 24}}
-                  name="session"
-                  rules={[{ required: true, message: '必填项' }]}
-                >
-                  {list.length > 0 ? (
-                    <Radio.Group>
-                      {currentRoom === '0' && (<>
-                        <Radio value={0}>16日（余{list[0].count}）</Radio>
-                        <Radio name="session" value={1}>17日（余{list[1].count}）</Radio>
-                      </>)}
-                      {currentRoom === '1' && (<>
-                        <Radio value={2}>18日（余{list[2].count}）</Radio>
-                        <Radio value={3}>19日（余{list[3].count}）</Radio>
-                      </>)}
-                      {currentRoom === '2' && (<>
-                        <Radio value={4}>20日（余{list[4].count}）</Radio>
-                      </>)}
-                      {currentRoom === '3' && (<>
-                        <Radio value={5}>21日（余{list[5].count}）</Radio>
-                        <Radio value={6}>22日（余{list[6].count}）</Radio>
-                      </>)}
-                    </Radio.Group>
-                  ) : <Text type='danger'>服务器开小差，请刷新重试</Text>}
-                </Form.Item>
-
-
-                {/*{list.length > 0 && (*/}
-                {/*  <>*/}
-                {/*    <Input.Group compact>*/}
-                {/*      <Form.Item noStyle>*/}
-                {/*        <Select placeholder="选择小区" defaultValue='yangzhu' onChange={val => setCurrentRoom(val)}>*/}
-                {/*          <Option value="yangzhu">央著</Option>*/}
-                {/*          <Option value="yangyu">央誉</Option>*/}
-                {/*          <Option value="yuhu">玉湖壹号</Option>*/}
-                {/*          <Option value="panlong">磐龙府</Option>*/}
-                {/*        </Select>*/}
-                {/*      </Form.Item>*/}
-                {/*      <br/>*/}
-                {/*      <br/>*/}
-                {/*      <Radio.Group onChange={val => setDate(val)}>*/}
-                {/*        {currentRoom === 'yangzhu' && (<>*/}
-                {/*          <Radio value={0}>16日（余{list[0].count}）</Radio>*/}
-                {/*          <Radio name="session" value={1}>17日（余{list[1].count}）</Radio>*/}
-                {/*        </>)}*/}
-                {/*        {currentRoom === 'yangyu' && (<>*/}
-                {/*          <Radio value={2}>18日（余{list[2].count}）</Radio>*/}
-                {/*          <Radio value={3}>19日（余{list[3].count}）</Radio>*/}
-                {/*        </>)}*/}
-                {/*        {currentRoom === 'yuhu' && (<>*/}
-                {/*          <Radio value={4}>20日（余{list[4].count}）</Radio>*/}
-                {/*        </>)}*/}
-                {/*        {currentRoom === 'panlong' && (<>*/}
-                {/*          <Radio value={5}>21日（余{list[5].count}）</Radio>*/}
-                {/*          <Radio value={6}>22日（余{list[6].count}）</Radio>*/}
-                {/*        </>)}*/}
-                {/*      </Radio.Group>*/}
-                {/*    </Input.Group>*/}
-                {/*    {!date && <Text type="danger">请选择日期</Text>}*/}
-                {/*  </>*/}
-                {/*)}*/}
-
+                {list.length > 0 ? (
+                  <Radio.Group>
+                    {currentRoom === '0' && (<>
+                      <Radio value={0}>16日（余{list[0].count}）</Radio>
+                      <Radio name="session" value={1}>17日（余{list[1].count}）</Radio>
+                    </>)}
+                    {currentRoom === '1' && (<>
+                      <Radio value={2}>18日（余{list[2].count}）</Radio>
+                      <Radio value={3}>19日（余{list[3].count}）</Radio>
+                    </>)}
+                    {currentRoom === '2' && (<>
+                      <Radio value={4}>20日（余{list[4].count}）</Radio>
+                    </>)}
+                    {currentRoom === '3' && (<>
+                      <Radio value={5}>21日（余{list[5].count}）</Radio>
+                      <Radio value={6}>22日（余{list[6].count}）</Radio>
+                    </>)}
+                  </Radio.Group>
+                ) : <Text type='danger'>服务器开小差，请刷新重试</Text>}
               </Form.Item>
 
 
-              <Form.Item>
-                <Flex justifyContent="center">
-                  <Button type="primary" htmlType="submit">
-                    提交
-                  </Button>
-                </Flex>
+              {/*{list.length > 0 && (*/}
+              {/*  <>*/}
+              {/*    <Input.Group compact>*/}
+              {/*      <Form.Item noStyle>*/}
+              {/*        <Select placeholder="选择小区" defaultValue='yangzhu' onChange={val => setCurrentRoom(val)}>*/}
+              {/*          <Option value="yangzhu">央著</Option>*/}
+              {/*          <Option value="yangyu">央誉</Option>*/}
+              {/*          <Option value="yuhu">玉湖壹号</Option>*/}
+              {/*          <Option value="panlong">磐龙府</Option>*/}
+              {/*        </Select>*/}
+              {/*      </Form.Item>*/}
+              {/*      <br/>*/}
+              {/*      <br/>*/}
+              {/*      <Radio.Group onChange={val => setDate(val)}>*/}
+              {/*        {currentRoom === 'yangzhu' && (<>*/}
+              {/*          <Radio value={0}>16日（余{list[0].count}）</Radio>*/}
+              {/*          <Radio name="session" value={1}>17日（余{list[1].count}）</Radio>*/}
+              {/*        </>)}*/}
+              {/*        {currentRoom === 'yangyu' && (<>*/}
+              {/*          <Radio value={2}>18日（余{list[2].count}）</Radio>*/}
+              {/*          <Radio value={3}>19日（余{list[3].count}）</Radio>*/}
+              {/*        </>)}*/}
+              {/*        {currentRoom === 'yuhu' && (<>*/}
+              {/*          <Radio value={4}>20日（余{list[4].count}）</Radio>*/}
+              {/*        </>)}*/}
+              {/*        {currentRoom === 'panlong' && (<>*/}
+              {/*          <Radio value={5}>21日（余{list[5].count}）</Radio>*/}
+              {/*          <Radio value={6}>22日（余{list[6].count}）</Radio>*/}
+              {/*        </>)}*/}
+              {/*      </Radio.Group>*/}
+              {/*    </Input.Group>*/}
+              {/*    {!date && <Text type="danger">请选择日期</Text>}*/}
+              {/*  </>*/}
+              {/*)}*/}
 
-              </Form.Item>
-            </Form>
-          </Flex>
+            </Form.Item>
+
+
+            <Form.Item>
+              <Flex justifyContent="center">
+                <Button type="primary" htmlType="submit">
+                  提交
+                </Button>
+              </Flex>
+
+            </Form.Item>
+          </Form>
         </Flex>
-      )}
-
+      </Flex>
     </>
   )
 }
